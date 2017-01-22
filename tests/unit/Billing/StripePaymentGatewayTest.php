f31cc7d47c2ead0de187c9dd61a6412fc031bd22
Max VelDink <maxveldink@gmail.com>
2017-01-22 07:27:31 -0500

    From 'Dealing with Lingering State'

---
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Billing\StripePaymentGateway;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class StipePaymentGatewayTest extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->lastCharge = $this->lastCharge();
+    }
+
+    private function lastCharge()
+    {
+        return $lastCharge = \Stripe\Charge::all(
+            ['limit' => 1],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'][0];
+    }
+
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                'limit' => 1,
+                'ending_before' => $this->lastCharge ? $this->lastCharge->id : null,
+            ],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'];
+    }
+
+    private function validToken()
+    {
+        return \Stripe\Token::create([
+            "card" => [
+                "number" => "4242424242424242",
+                "exp_month" => 1,
+                "exp_year" => date('Y') + 1,
+                "cvc" => "123",
+            ]
+        ], ['api_key' => config('services.stripe.secret')])->id;
+    }
+
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful() 
+    {
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+
+        $paymentGateway->charge(2500, $this->validToken());
+
+        $this->assertCount(1, $this->newCharges());
+        $this->assertEquals(2500, $this->lastCharge()->amount);
+    }
+}
