2f3ed5f04c8d9926a09b1436554da480b729bc16
Max VelDink <maxveldink@gmail.com>
2017-01-24 09:55:33 -0500

    From 'Extracting the Invalid Token Test'

--- app/Billing/StripePaymentGateway.php
+++ app/Billing/StripePaymentGateway.php
@@ -3,6 +3,8 @@
 namespace App\Billing;
 
 use Stripe\Charge;
+use Stripe\Error\InvalidRequest;
+use App\Billing\PaymentFailedException;
 
 class StripePaymentGateway implements PaymentGateway
 {
@@ -12,13 +14,54 @@ class StripePaymentGateway implements PaymentGateway
     {
         $this->apiKey = $apiKey;
     }
-    
+
     public function charge($amount, $token)
     {
-        Charge::create([
-            'amount' => $amount,
-            'source' => $token,
-            'currency' => 'usd',
-        ], $this->apiKey);
+        try {
+            Charge::create([
+                'amount' => $amount,
+                'source' => $token,
+                'currency' => 'usd',
+            ], $this->apiKey);
+        } catch (InvalidRequest $e) {
+            throw new PaymentFailedException;
+        }
+    }
+
+    public function getValidTestToken()
+    {
+        return \Stripe\Token::create([
+            "card" => [
+                "number" => "4242424242424242",
+                "exp_month" => 1,
+                "exp_year" => date('Y') + 1,
+                "cvc" => "123",
+            ]
+        ], ['api_key' => $this->apiKey]);
+    }
+
+    public function newChargesDuring($callback)
+    {
+        $latestCharge = $this->lastCharge();
+        $callback($this);
+        return $this->newChargesSince($latestCharge)->pluck('amount');
+    }
+
+    public function lastCharge()
+    {
+        return array_first($lastCharge = \Stripe\Charge::all(
+            ['limit' => 1],
+            ['api_key' => $this->apiKey]
+        )['data']);
+    }
+
+    private function newChargesSince($charge = null)
+    {
+        $newCharges = \Stripe\Charge::all(
+            [ 'ending_before' => $charge ? $charge->id : null ],
+            ['api_key' => $this->apiKey]
+        )['data'];
+
+        return collect($newCharges);
     }
 }
--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -1,57 +1,20 @@
 <?php
 
 use App\Billing\StripePaymentGateway;
+use App\Billing\PaymentFailedException;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+/** 
+ * @group integration
+ */
 class StipePaymentGatewayTest extends TestCase
 {
-    protected function setUp()
+    use PaymentGatewayContractTests;
+
+    protected function getPaymentGateway()
     {
-        parent::setUp();
-        $this->lastCharge = $this->lastCharge();
-    }
-
-    private function lastCharge()
-    {
-        return $lastCharge = \Stripe\Charge::all(
-            ['limit' => 1],
-            ['api_key' => config('services.stripe.secret')]
-        )['data'][0];
-    }
-
-    private function newCharges()
-    {
-        return \Stripe\Charge::all(
-            [
-                'limit' => 1,
-                'ending_before' => $this->lastCharge ? $this->lastCharge->id : null,
-            ],
-            ['api_key' => config('services.stripe.secret')]
-        )['data'];
-    }
-
-    private function validToken()
-    {
-        return \Stripe\Token::create([
-            "card" => [
-                "number" => "4242424242424242",
-                "exp_month" => 1,
-                "exp_year" => date('Y') + 1,
-                "cvc" => "123",
-            ]
-        ], ['api_key' => config('services.stripe.secret')])->id;
-    }
-
-    /** @test */
-    function charges_with_a_valid_payment_token_are_successful() 
-    {
-        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
-
-        $paymentGateway->charge(2500, $this->validToken());
-
-        $this->assertCount(1, $this->newCharges());
-        $this->assertEquals(2500, $this->lastCharge()->amount);
+        return new StripePaymentGateway(config('services.stripe.secret'));
     }
 }
